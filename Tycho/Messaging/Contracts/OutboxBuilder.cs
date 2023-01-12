38b812678ac8d40788537b9d5a0f720e01cc6c3c
Bartosz Pawluk <pawlukb@outlook.com>
2023-01-12 23:09:57 +0100

    Validate module outbox contract before building it

--- Tycho/Messaging/Contracts/OutboxBuilder.cs
+++ Tycho/Messaging/Contracts/OutboxBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Tycho.Messaging.Handlers;
@@ -7,17 +8,15 @@ using Tycho.Messaging.Payload;
 
 namespace Tycho.Messaging.Contracts
 {
-    internal class OutboxBuilder : IOutboxDefiner, IOutboxConsumer, IOutboxBuilder, IDisposable
+    internal class OutboxBuilder : IOutboxDefiner, IOutboxConsumer, IOutboxBuilder
     {
         private readonly IMessageRouter _moduleInbox;
-        private readonly ReaderWriterLockSlim _lock;
-        private readonly HashSet<Type> _definedMessages;
+        private readonly ConcurrentDictionary<Type, bool> _messageRegistry;
 
         public OutboxBuilder(IMessageRouter moduleInbox)
         {
             _moduleInbox = moduleInbox;
-            _lock = new ReaderWriterLockSlim();
-            _definedMessages = new HashSet<Type>();
+            _messageRegistry = new ConcurrentDictionary<Type, bool>();
         }
 
         #region IOutboxDefiner
@@ -47,45 +46,48 @@ namespace Tycho.Messaging.Contracts
         public IOutboxConsumer OnEvent<Event>(Action<Event> action)
             where Event : class, IEvent
         {
-            ValidateIfMessageIsDefined(typeof(Event), nameof(Event));
             var handler = new LambdaWrappingEventHandler<Event>(action);
-            _moduleInbox.RegisterEventHandler(handler);
+            RegisterEventHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnEvent<Event>(Func<Event, Task> function)
             where Event : class, IEvent
         {
-            ValidateIfMessageIsDefined(typeof(Event), nameof(Event));
             var handler = new LambdaWrappingEventHandler<Event>(function);
-            _moduleInbox.RegisterEventHandler(handler);
+            RegisterEventHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnEvent<Event>(Func<Event, CancellationToken, Task> function)
             where Event : class, IEvent
         {
-            ValidateIfMessageIsDefined(typeof(Event), nameof(Event));
             var handler = new LambdaWrappingEventHandler<Event>(function);
-            _moduleInbox.RegisterEventHandler(handler);
+            RegisterEventHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnEvent<Event>(IEventHandler<Event> handler)
             where Event : class, IEvent
         {
-            ValidateIfMessageIsDefined(typeof(Event), nameof(Event));
-            _moduleInbox.RegisterEventHandler(handler);
+            RegisterEventHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnEvent<Event>(Func<IEventHandler<Event>> handlerCreator)
             where Event : class, IEvent
         {
-            ValidateIfMessageIsDefined(typeof(Event), nameof(Event));
             var handler = new TransientEventHandler<Event>(handlerCreator);
-            _moduleInbox.RegisterEventHandler(handler);
+            RegisterEventHandler(handler);
             return this;
+        }
+
+        private void RegisterEventHandler<Event>(IEventHandler<Event> handler)
+            where Event : class, IEvent
+        {
+            ValidateIfMessageIsDefined(typeof(Event), nameof(Event));
+            _moduleInbox.RegisterEventHandler(handler);
+            MarkMessageAsHandled(typeof(Event));
         }
         #endregion
 
@@ -93,54 +95,56 @@ namespace Tycho.Messaging.Contracts
         public IOutboxConsumer OnCommand<Command>(Action<Command> action)
             where Command : class, ICommand
         {
-            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
             var handler = new LambdaWrappingCommandHandler<Command>(action);
-            _moduleInbox.RegisterCommandHandler(handler);
+            RegisterCommandHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnCommand<Command>(Func<Command, Task> function)
             where Command : class, ICommand
         {
-            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
             var handler = new LambdaWrappingCommandHandler<Command>(function);
-            _moduleInbox.RegisterCommandHandler(handler);
+            RegisterCommandHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnCommand<Command>(Func<Command, CancellationToken, Task> function)
             where Command : class, ICommand
         {
-            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
             var handler = new LambdaWrappingCommandHandler<Command>(function);
-            _moduleInbox.RegisterCommandHandler(handler);
+            RegisterCommandHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnCommand<Command>(ICommandHandler<Command> handler)
             where Command : class, ICommand
         {
-            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
-            _moduleInbox.RegisterCommandHandler(handler);
+            RegisterCommandHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnCommand<Command>(Func<ICommandHandler<Command>> handlerCreator)
             where Command : class, ICommand
         {
-            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
             var handler = new TransientCommandHandler<Command>(handlerCreator);
-            _moduleInbox.RegisterCommandHandler(handler);
+            RegisterCommandHandler(handler);
             return this;
         }
 
         public IOutboxConsumer Ignore<Command>()
             where Command : class, ICommand
         {
-            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
             var handler = new StubCommandHandler<Command>();
-            _moduleInbox.RegisterCommandHandler(handler);
+            RegisterCommandHandler(handler);
             return this;
+        }
+
+        private void RegisterCommandHandler<Command>(ICommandHandler<Command> handler)
+            where Command : class, ICommand
+        {
+            ValidateIfMessageIsDefined(typeof(Command), nameof(Command));
+            _moduleInbox.RegisterCommandHandler(handler);
+            MarkMessageAsHandled(typeof(Command));
         }
         #endregion
 
@@ -148,107 +152,100 @@ namespace Tycho.Messaging.Contracts
         public IOutboxConsumer OnQuery<Query, Response>(Func<Query, Response> function)
             where Query : class, IQuery<Response>
         {
-            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
             var handler = new LambdaWrappingQueryHandler<Query, Response>(function);
-            _moduleInbox.RegisterQueryHandler(handler);
+            RegisterQueryHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnQuery<Query, Response>(Func<Query, Task<Response>> function)
             where Query : class, IQuery<Response>
         {
-            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
             var handler = new LambdaWrappingQueryHandler<Query, Response>(function);
-            _moduleInbox.RegisterQueryHandler(handler);
+            RegisterQueryHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnQuery<Query, Response>(Func<Query, CancellationToken, Task<Response>> function)
             where Query : class, IQuery<Response>
         {
-            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
             var handler = new LambdaWrappingQueryHandler<Query, Response>(function);
-            _moduleInbox.RegisterQueryHandler(handler);
+            RegisterQueryHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnQuery<Query, Response>(IQueryHandler<Query, Response> handler)
             where Query : class, IQuery<Response>
         {
-            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
-            _moduleInbox.RegisterQueryHandler(handler);
+            RegisterQueryHandler(handler);
             return this;
         }
 
         public IOutboxConsumer OnQuery<Query, Response>(Func<IQueryHandler<Query, Response>> handlerCreator)
             where Query : class, IQuery<Response>
         {
-            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
             var handler = new TransientQueryHandler<Query, Response>(handlerCreator);
-            _moduleInbox.RegisterQueryHandler(handler);
+            RegisterQueryHandler(handler);
             return this;
         }
 
         public IOutboxConsumer Return<Query, Response>(Response response)
             where Query : class, IQuery<Response>
         {
-            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
             var handler = new StubQueryHandler<Query, Response>(response);
+            RegisterQueryHandler(handler);
+            return this;
+        }
+
+        public IOutboxConsumer RegisterQueryHandler<Query, Response>(IQueryHandler<Query, Response> handler)
+            where Query : class, IQuery<Response>
+        {
+            ValidateIfMessageIsDefined(typeof(Query), nameof(Query));
             _moduleInbox.RegisterQueryHandler(handler);
+            MarkMessageAsHandled(typeof(Query));
             return this;
         }
         #endregion
 
         public IMessageBroker Build()
         {
-            // TODO: Make sure all defined commands and queries have handlers
+            ValidateIfAllMethodsAreHandled();
             return new MessageBroker(_moduleInbox);
-        }
-
-        public void Dispose()
-        {
-            _lock.Dispose();
-        }
-
-        private void ValidateIfMessageIsDefined(Type messageType, string messageKind)
-        {
-            if (!IsMessageDefined(messageType))
-            {
-                throw new InvalidOperationException($"Could not register the handler because the {messageType.Name} {messageKind} is not defined for this module");
-            }
-        }
-
-        private bool IsMessageDefined(Type messageType)
-        {
-            _lock.EnterReadLock();
-            try
-            {
-                return _definedMessages.Contains(messageType);
-            }
-            finally
-            {
-                _lock.ExitReadLock();
-            }
         }
 
         private void AddMessageDefinition(Type messageType, string messageKind)
         {
-            var added = false;
-
-            _lock.EnterWriteLock();
-            try
+            if (!_messageRegistry.TryAdd(messageType, false))
             {
-                added = _definedMessages.Add(messageType);
-            }
-            finally
-            {
-                _lock.ExitWriteLock();
-            }
-
-            if (!added)
-            {
-                throw new ArgumentException($"The {messageType.Name} {messageKind} is already defined for this module", nameof(messageType));
+                throw new ArgumentException(
+                    $"The {messageType.Name} {messageKind} is already defined for this module", 
+                    nameof(messageType));
             }
         }
+
+        private void ValidateIfMessageIsDefined(Type messageType, string messageKind)
+        {
+            if (!_messageRegistry.ContainsKey(messageType))
+            {
+                throw new InvalidOperationException(
+                    $"Could not register a message handler because " +
+                    $"the {messageType.Name} {messageKind} is not defined for this module");
+            }
+        }
+
+        private void ValidateIfAllMethodsAreHandled()
+        {
+            var missingHandlers = _messageRegistry.Where(entry => !entry.Value && ShouldMessageBeHandled(entry.Key));
+            if (missingHandlers.Any())
+            {
+                throw new InvalidOperationException(
+                    $"Could not construct a message broker for this module " +
+                    $"because one or more messages are missing required handlers. " +
+                    $"Please provide handler for: {string.Join(',', missingHandlers.Select(entry => entry.Key))}");
+            }
+        }
+
+        private void MarkMessageAsHandled(Type type) => _messageRegistry.TryUpdate(type, true, false);
+
+        private bool ShouldMessageBeHandled(Type type) => !typeof(IEvent).IsAssignableFrom(type);
     }
 }
