c5e2ed6edbc94639cfec7470b9a2d3db72c8abe2
Bartosz Pawluk <pawlukb@outlook.com>
2023-01-22 12:06:08 +0100

    Documentation comments for substructure definition

--- Tycho/Structure/ISubstructureDefinition.cs
+++ Tycho/Structure/ISubstructureDefinition.cs
@@ -3,12 +3,26 @@ using Tycho.Contract;
 
 namespace Tycho.Structure
 {
+    /// <summary>
+    /// Lets you define which submodules will be part of your module and how it will use them
+    /// </summary>
     public interface ISubstructureDefinition
     {
+        /// <summary>
+        /// Adds the specified module to the substructure of your module
+        /// </summary>
+        /// <typeparam name="Module">A type that defines the submodule</typeparam>
         ISubstructureDefinition AddSubmodule<Module>()
             where Module : TychoModule, new();
 
-        ISubstructureDefinition AddSubmodule<Module>(Action<IOutboxConsumer> consumeMessages)
+        /// <summary>
+        /// Adds the specified module to the substructure of your module 
+        /// and defines how it handles its outgoing messages
+        /// </summary>
+        /// <typeparam name="Module">A type that defines the submodule</typeparam>
+        /// <param name="contractFulfillment">A method that defines 
+        /// how your module fulfills the submodule's contract</param>
+        ISubstructureDefinition AddSubmodule<Module>(Action<IOutboxConsumer> contractFulfillment)
             where Module : TychoModule, new();
     }
 }
