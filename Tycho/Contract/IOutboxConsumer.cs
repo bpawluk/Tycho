8376157dd50f9257f22510f9ef92b3d5a8dbbbe4
Bartosz Pawluk <pawlukb@outlook.com>
2023-01-22 23:37:10 +0100

    Documentation comments for outbox definition and consumer

--- Tycho/Contract/IOutboxConsumer.cs
+++ Tycho/Contract/IOutboxConsumer.cs
@@ -6,64 +6,191 @@ using Tycho.Messaging.Payload;
 
 namespace Tycho.Contract
 {
+    /// <summary>
+    /// Lets you define logic for handling messages sent out by a module you want to use
+    /// </summary>
     public interface IOutboxConsumer
     {
         #region Events
+        /// <summary>
+        /// Defines logic for handling the specified <b>event</b> message
+        /// </summary>
+        /// <typeparam name="Event">The type of the event being handled</typeparam>
+        /// <param name="action">A method to be invoked when the event is published</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleEvent<Event>(Action<Event> action)
             where Event : class, IEvent;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>event</b> message
+        /// </summary>
+        /// <typeparam name="Event">The type of the event being handled</typeparam>
+        /// <param name="function">A method to be invoked when the event is published</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleEvent<Event>(Func<Event, Task> function)
             where Event : class, IEvent;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>event</b> message
+        /// </summary>
+        /// <typeparam name="Event">The type of the event being handled</typeparam>
+        /// <param name="function">A method to be invoked when the event is published</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleEvent<Event>(Func<Event, CancellationToken, Task> function)
             where Event : class, IEvent;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>event</b> message
+        /// </summary>
+        /// <typeparam name="Event">The type of the event being handled</typeparam>
+        /// <param name="handler">A handler to be used when the event is published</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleEvent<Event>(IEventHandler<Event> handler)
             where Event : class, IEvent;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>event</b> message
+        /// </summary>
+        /// <typeparam name="Event">The type of the event being handled</typeparam>
+        /// <typeparam name="Handler">A handler to be used when the event is published</typeparam>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleEvent<Event, Handler>()
             where Handler : class, IEventHandler<Event>
             where Event : class, IEvent;
         #endregion
 
         #region Commands
+        /// <summary>
+        /// Defines logic for handling the specified <b>command</b> message
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being handled</typeparam>
+        /// <param name="action">A method to be invoked when the command is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleCommand<Command>(Action<Command> action)
             where Command : class, ICommand;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>command</b> message
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being handled</typeparam>
+        /// <param name="function">A method to be invoked when the command is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleCommand<Command>(Func<Command, Task> function)
             where Command : class, ICommand;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>command</b> message
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being handled</typeparam>
+        /// <param name="function">A method to be invoked when the command is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleCommand<Command>(Func<Command, CancellationToken, Task> function)
             where Command : class, ICommand;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>command</b> message
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being handled</typeparam>
+        /// <param name="handler">A handler to be used when the command is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleCommand<Command>(ICommandHandler<Command> handler)
             where Command : class, ICommand;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>command</b> message
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being handled</typeparam>
+        /// <typeparam name="Handler">A handler to be used when the command is received</typeparam>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleCommand<Command, Handler>()
             where Handler : class, ICommandHandler<Command>
             where Command : class, ICommand;
 
+        /// <summary>
+        /// Ignores the specified <b>command</b> message
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being ignored</typeparam>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer IgnoreCommand<Command>()
             where Command : class, ICommand;
         #endregion
 
         #region Queries
+        /// <summary>
+        /// Defines logic for handling the specified <b>query</b> message
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being handled</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
+        /// <param name="function">A method to be invoked when the query is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleQuery<Query, Response>(Func<Query, Response> function)
             where Query : class, IQuery<Response>;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>query</b> message
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being handled</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
+        /// <param name="function">A method to be invoked when the query is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleQuery<Query, Response>(Func<Query, Task<Response>> function)
             where Query : class, IQuery<Response>;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>query</b> message
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being handled</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
+        /// <param name="function">A method to be invoked when the query is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleQuery<Query, Response>(Func<Query, CancellationToken, Task<Response>> function)
             where Query : class, IQuery<Response>;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>query</b> message
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being handled</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
+        /// <param name="handler">A handler to be used when the query is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleQuery<Query, Response>(IQueryHandler<Query, Response> handler)
             where Query : class, IQuery<Response>;
 
+        /// <summary>
+        /// Defines logic for handling the specified <b>query</b> message
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being handled</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
+        /// <typeparam name="Handler">A handler to be used when the query is received</typeparam>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer HandleQuery<Query, Response, Handler>()
             where Handler : class, IQueryHandler<Query, Response>
             where Query : class, IQuery<Response>;
 
+        /// <summary>
+        /// Ignores the specified <b>query</b> message and returns the provided default response
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being ignored</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
+        /// <param name="response">The default response to return when the query is received</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         IOutboxConsumer IgnoreQuery<Query, Response>(Response response)
             where Query : class, IQuery<Response>;
         #endregion
--- Tycho/Contract/IOutboxDefinition.cs
+++ Tycho/Contract/IOutboxDefinition.cs
@@ -2,14 +2,31 @@
 
 namespace Tycho.Contract
 {
+    /// <summary>
+    /// Lets you define all the outgoing messages that your module will send out.
+    /// These messages make up the contract that your module requires its clients to fulfill
+    /// </summary>
     public interface IOutboxDefinition
     {
+        /// <summary>
+        /// Declares that the specified <b>event</b> message is published by your module
+        /// </summary>
+        /// <typeparam name="Event">The type of the event being published</typeparam>
         IOutboxDefinition Publishes<Event>()
             where Event : class, IEvent;
 
+        /// <summary>
+        /// Declares that the specified <b>command</b> message is sent by your module
+        /// </summary>
+        /// <typeparam name="Command">The type of the command being sent</typeparam>
         IOutboxDefinition Sends<Command>()
             where Command : class, ICommand;
 
+        /// <summary>
+        /// Declares that the specified <b>query</b> message is sent by your module
+        /// </summary>
+        /// <typeparam name="Query">The type of the query being sent</typeparam>
+        /// <typeparam name="Response">The type of the query response</typeparam>
         IOutboxDefinition Sends<Query, Response>()
             where Query : class, IQuery<Response>;
     }
