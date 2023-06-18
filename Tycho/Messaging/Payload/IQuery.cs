3b15b058dd9b775b506fab91e1bce4b6d8f2c628
Bartosz Pawluk <pawlukb@outlook.com>
2023-06-18 13:36:38 +0200

    IQuery interface variance

--- Tycho/Messaging/Payload/IQuery.cs
+++ Tycho/Messaging/Payload/IQuery.cs
@@ -4,5 +4,5 @@
     /// An interface that represents a query message
     /// </summary>
     /// <typeparam name="Response">The type of the query response</typeparam>
-    public interface IQuery<Response> { }
+    public interface IQuery<out Response> { }
 }
