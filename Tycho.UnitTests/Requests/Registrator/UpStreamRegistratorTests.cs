bf5a00b10c96b348242ea5d31aefb3935ad98365
Bartosz <pawlukb@outlook.com>
2024-10-03 18:04:05 +0200

    wip: progressing

--- Tycho.UnitTests/Requests/Registrator/UpStreamRegistratorTests.cs
+++ Tycho.UnitTests/Requests/Registrator/UpStreamRegistratorTests.cs
@@ -2,4 +2,83 @@
 
 public class UpStreamRegistratorTests
 {
+    [Fact]
+    public void Forward_NewRequest_RegistersForwarder()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Forward_ExistingRequest_ThrowsArgumentException()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Forward_NewRequestWithResponse_RegistersForwarder()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Forward_ExistingRequestWithResponse_ThrowsArgumentException()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Handle_NewRequest_RegistersHandler()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Handle_ExistingRequest_ThrowsArgumentException()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Handle_NewRequestWithResponse_RegistersHandler()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void Handle_ExistingRequestWithResponse_ThrowsArgumentException()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
 }
---
+++ Tycho.UnitTests/_Data/Handlers/TestRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using Tycho.UnitTests._Data.Requests;
+using TychoV2.Requests;
+
+namespace Tycho.UnitTests._Data.Handlers;
+
+internal class TestRequestHandler
+    : IHandle<TestRequest>
+    , IHandle<TestRequestWithResponse, string>
+{
+    public Task Handle(TestRequest requestData, CancellationToken cancellationToken) =>
+        Task.CompletedTask;
+
+    public Task<string> Handle(TestRequestWithResponse requestData, CancellationToken cancellationToken ) =>
+        Task.FromResult(default(string)!);
+}
