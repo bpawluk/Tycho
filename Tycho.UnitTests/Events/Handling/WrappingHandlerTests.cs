04299848937bad31fc8e4cae763b0608db31c521
Bartosz <pawlukb@outlook.com>
2024-10-18 08:01:44 +0200

    wip: one more test

---
+++ Tycho.UnitTests/Events/Handling/WrappingHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using Tycho.Events;
+using Tycho.Events.Handling;
+using Tycho.UnitTests._Data.Events;
+
+namespace Tycho.UnitTests.Events.Handling;
+
+public class WrappingHandlerTests
+{
+    [Fact]
+    public async Task Handle_InvokesWrappedHandlerWithMappedEventData()
+    {
+        // Arrange
+        var eventData = new TestEvent();
+        var mappedEventData = new OtherEvent();
+
+        var wrappedHandler = new Mock<IEventHandler<OtherEvent>>();
+        var map = new Mock<Func<TestEvent, OtherEvent>>();
+        map.Setup(m => m(eventData)).Returns(mappedEventData);
+
+        var sut = new WrappingHandler<TestEvent, OtherEvent>(wrappedHandler.Object, map.Object);
+
+        // Act
+        await sut.Handle(eventData, CancellationToken.None);
+
+        // Assert
+        wrappedHandler.Verify(w => w.Handle(mappedEventData, CancellationToken.None), Times.Once);
+        map.Verify(m => m(eventData), Times.Once);
+    }
+}
