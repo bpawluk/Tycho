98441be312bebb92a06505b768976c902068ab29
Bartosz <pawlukb@outlook.com>
2024-10-21 12:11:46 +0200

    efc: project structure

---
+++ Extensions/Tycho.Persistence.EFCore/Outbox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tycho.Persistence.EFCore;
+
+internal class Outbox : IOutbox
+{
+    public event EventHandler? NewEntriesAdded;
+
+    public Task Add(IReadOnlyCollection<OutboxEntry> entries, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyCollection<OutboxEntry>> Read(int count, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task MarkAsProcessed(OutboxEntry entry, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task MarkAsFailed(OutboxEntry entry, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
