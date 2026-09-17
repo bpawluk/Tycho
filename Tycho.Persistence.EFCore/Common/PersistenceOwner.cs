9212c997336a03d89c44980b4ae9e830bf1da54b
Bartosz Pawluk <pawlukb@outlook.com>
2026-09-17 22:14:41 +0200

    wip

--- Tycho.Persistence.EFCore/Common/PersistenceOwner.cs
+++ Tycho.Persistence.EFCore/Common/PersistenceOwner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.Extensions.Logging;
 using Tycho.Structure;
 
 namespace Tycho.Persistence.EFCore.Common;
@@ -9,10 +10,16 @@ internal sealed class PersistenceOwner
 {
     public string Key { get; }
 
-    public PersistenceOwner(Internals internals)
+    public PersistenceOwner(Internals internals, ILogger<PersistenceOwner>? logger = null)
     {
         ArgumentNullException.ThrowIfNull(internals);
+
         byte[] hash = SHA256.HashData(Encoding.UTF8.GetBytes(internals.OwnerInstanceId.Value));
         Key = Convert.ToHexString(hash.AsSpan(0, 16));
+
+        if (logger?.IsEnabled(LogLevel.Information) == true)
+        {
+            logger.LogInformation("Persistence key {OwnerKey} configured for {OwnerInstanceId}", Key, internals.OwnerInstanceId.Value);
+        }
     }
 }
